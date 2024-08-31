e2850bd8f450e6da3a1e3fcaf5af3423b4685e8f
qTsai <qingyetsai@hotmail.com>
2024-08-31 18:51:25 +0200

    init project

---
+++ src/main/kotlin/nl/tsai/cabin_cache/CabinCacheApplication.kt
@@ -0,0 +1,11 @@
+package nl.tsai.cabin_cache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CabinCacheApplication
+
+fun main(args: Array<String>) {
+	runApplication<CabinCacheApplication>(*args)
+}
---
+++ src/test/kotlin/nl/tsai/cabin_cache/CabinCacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.tsai.cabin_cache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CabinCacheApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
